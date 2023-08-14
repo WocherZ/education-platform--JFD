33e1431a13e96dce24a2ce3a33321e33da9818f8
afinogenka <afinogenkan1@mail.com>
2023-08-14 14:28:22 +0300

    chenged categories jsx

--- education-platform/src/components/main_page/Categories.jsx
+++ education-platform/src/components/main_page/Categories.jsx
@@ -1,40 +1,65 @@
-import './categories.css';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@mui/material';
+import { CssBaseline, Container, Typography } from '@mui/material';
+import React from 'react';
 import Cards from './Cards';
 
+const styles = {
+  container: {
+    backgroundColor: '#FAFEFF',
+    paddingTop: '70px',
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    paddingBottom: '93px',
+    textAlign: 'start',
+  },
+  header: {
+    color: '#00ABBD',
+    fontFamily: 'Poppins',
+    fontSize: '20px',
+    fontWeight: 500,
+  },
+  text: {
+    color: '#5F6368',
+    fontFamily: 'Poppins',
+    fontSize: '40px',
+    fontWeight: 700,
+    paddingTop: '10px',
+  },
+  link: {
+    color: '#96989B',
+    fontFamily: 'Poppins',
+    fontSize: '16px',
+    fontWeight: 500,
+    lineHeight: 'normal',
+    textDecoration: 'none',
+  },
+};
+
 const Categories = () => {
-
-    return (
-       <>
-            <CssBaseline />
-            <div className="categories">
-                <Container className='categories_container'>
-                    <Typography variant='h6' textAlign='start' className='categories_header'>Popular Categories</Typography>
-                    <Typography variant='h3' textAlign='start' className='categories_text'>Explore the most popular categories</Typography>
-                    <div className='links_to'>
-                        <ul className='links_to__group'>
-                            <li className='left_link'><a></a></li>
-                            <li className='right_link'><a></a></li>
-                        </ul>
-                    </div>
-
-                    <div className='courses_container'>
-                        <ul className='courses_list'>
-                            <li><a href='#' className='courses'>Programming</a></li>
-                            <li><a href='#' className='courses'>Design</a></li>
-                            <li><a href='#' className='courses'>Bussiness</a></li>
-                            <li><a href='#' className='courses'>Lifestyle</a></li>
-                            <li><a href='#' className='courses'>Health & Fitness</a></li>
-                            <li><a href='#' className='courses'>Marketing</a></li>
-                            <li><a href='#' className='courses'>Music</a></li>
-                            <li><a href='#' className='courses'>Finance and Accounting</a></li>
-                        </ul>
-                    </div>
-                    <Cards></Cards>
-                </Container>
-            </div>
-       </>
-       );
+  return (
+    <>
+      <CssBaseline />
+      <div>
+        <Container style={styles.container}>
+          <Typography variant='h6' style={styles.header}>Popular Categories</Typography>
+          <Typography variant='h3' style={styles.text}>Explore the most popular categories</Typography>
+          <div style={{ padding: '17px 0 93px 0' }}>
+            <ul style={{ display: 'flex', flexWrap: 'wrap', gap: '15px', justifyContent: 'space-between' }}>
+            <li><a href='#' style={styles.link}>Programming</a></li>
+            <li><a href='#' style={styles.link}>Design</a></li>
+            <li><a href='#' style={styles.link}>Bussiness</a></li>
+            <li><a href='#' style={styles.link}>Lifestyle</a></li>
+            <li><a href='#' style={styles.link}>Health & Fitness</a></li>
+            <li><a href='#' style={styles.link}>Marketing</a></li>
+            <li><a href='#' style={styles.link}>Music</a></li>
+            <li><a href='#' style={styles.link}>Finance and Accounting</a></li>
+            </ul>
+          </div>
+          <Cards />
+        </Container>
+      </div>
+    </>
+  );
 }
 
 export default Categories;
+
